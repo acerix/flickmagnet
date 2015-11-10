a430f6a5a445859b08b52d456c3728144a1fd963
acerix <dylan@psilly.com>
2015-11-09 20:12:57 -0500

    script to delete user files

--- flickmagnet.sh
+++ flickmagnet.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
 /usr/bin/python3 /usr/lib/python3.5/site-packages/flickmagnet/flickmagnet.py
-
---
+++ reset.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# Delete config, cache, logs for the current user
+rm -r ~/.config/flickmagnet/ ~/.cache/flickmagnet/ ~/.local/share/flickmagnet/
+
